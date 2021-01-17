9c58ef5244c0e14057693b1e0fffcf6561473834
leonardo-g-o <leonardo-g-o@hotmail.com>
2021-01-17 15:50:56 -0400

    creating model of users

--- src/main/java/com/poligym/models/User.java
+++ src/main/java/com/poligym/models/User.java
@@ -27,6 +27,9 @@ public class User{
     @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
+    private String password;
+
     @Column(name = "registrationCode")
     private String registrationCode;
 
