bc988732f6b5c2eb81215a244fceca1c25a80f7a
leonardo-g-o <leonardo-g-o@hotmail.com>
2021-01-17 16:19:19 -0400

    Creating entity base

---
+++ src/main/java/com/poligym/models/EntityBase.java
@@ -0,0 +1,36 @@
+package com.poligym.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class EntityBase implements Serializable {    
+    private static final long serialVersionUID = 1L;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    @Column(nullable = true)
+    private Date updatedAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    @Column(nullable = true)
+    private Date removedAt;
+    
+}
