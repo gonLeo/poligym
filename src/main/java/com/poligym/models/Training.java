6a9fcbdd2b975a59f53dca1b223d93cd1663413c
Pedro <pedrinhozenere99@gmail.com>
2021-01-17 17:50:56 -0400

    finalizando model de Trainning

--- src/main/java/com/poligym/models/Training.java
+++ src/main/java/com/poligym/models/Training.java
@@ -1,15 +1,23 @@
 package com.poligym.models;
 
-import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import com.poligym.dto.TrainningDTO;
+
+import org.modelmapper.ModelMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,30 +35,38 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Training implements Serializable {
+public class Training extends EntityBase {
 
   /**
    *
    */
   private static final long serialVersionUID = 8318488844661779630L;
 
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Id
   @Column(name = "id")
   private int id;
 
-  @Column(name = "user_id")
-  private int user_id;
+  @JoinColumn(name = "user_id")
+  @ManyToOne
+  private User user;
 
-  @Column(name = "exercise_id")
-  private int exercise_id;
+  @JoinColumn(name = "exercise_id")
+  @OneToMany(mappedBy = "Trainning", targetEntity = Exercices.class, cascade = CascadeType.ALL)
+  private Exercices exercises;
 
+  @NotBlank
+  @Size(max = 50)
   @Column(name = "weight")
   private float weight;
 
+  @NotBlank
+  @Size(max = 50)
   @Column(name = "series")
   private float series;
 
+  @NotBlank
+  @Size(max = 50)
   @Column(name = "training_validity")
   private Date training_validity;
 
