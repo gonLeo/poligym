4c10a96f6b5d1a0f8d5ed6fddee290301c7a65c3
Pedro <pedrinhozenere99@gmail.com>
2021-01-17 16:20:06 -0400

    criando model de Usuario

--- src/main/java/com/poligym/models/Training.java
+++ src/main/java/com/poligym/models/Training.java
@@ -1,5 +1,70 @@
 package com.poligym.models;
 
-public class Training {
-    
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.poligym.dto.TrainningDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Class that implements an Trainning entity in the API.
+ * 
+ * @author Pedro Vinicius
+ * @since 17/01/2021
+ */
+
+@Entity
+@Table(name = "Trainning")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Training implements Serializable {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 8318488844661779630L;
+
+  @GeneratedValue
+  @Id
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "user_id")
+  private int user_id;
+
+  @Column(name = "exercise_id")
+  private int exercise_id;
+
+  @Column(name = "weight")
+  private float weight;
+
+  @Column(name = "series")
+  private float series;
+
+  @Column(name = "training_validity")
+  private Date training_validity;
+
+  /**
+   * Method to convert an Trainning entity to an User DTO
+   * 
+   * @author Pedro Vinicius
+   * @since 17/01/2021
+   * 
+   * @param person
+   * @return an TrainningDTO object
+   */
+
+  public TrainningDTO convertEntityToDTO() {
+    return new ModelMapper().map(this, TrainningDTO.class);
+  }
 }
