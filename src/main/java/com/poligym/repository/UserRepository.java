b09ba4fd41f69923d05c1c6f5fdeb51f13e4152c
Leonardo Gonçalves de Oliveira <leonardo.g.o.lrv@gmail.com>
2021-01-19 12:51:25 -0400

    Adicionando UserRepository

--- src/main/java/com/poligym/repository/UserRepository.java
+++ src/main/java/com/poligym/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.poligym.repository;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import com.poligym.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findById(int id);
 }
